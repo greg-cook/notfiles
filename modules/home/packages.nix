e568f7aa11b783826567bd5619befebce842cb7b
Greg Cook <5386965+greg-cook@users.noreply.github.com>
2026-02-02 21:38:47 +1300

    remove unused packages

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -8,36 +8,25 @@
   home.packages =
     with pkgs;
     [
-      autoconf
       claude-code
       fd
       fzf
-      gemini-cli
       gh
-      helix
-      lazydocker
-      luarocks
       mise
       mkcert
       mariadb.client
       nil
       openssl
-      pkg-config
-      pwgen
       python3
-      readline
       ripgrep
       ruby
       sslscan
-      tflint
-      totp-cli
-      treefmt
+      uv
       wakeonlan
       watch
       wget
       xz
       yq-go
-      zlib
     ]
     ++ (hostConfig.extraPackages pkgs);
 }
