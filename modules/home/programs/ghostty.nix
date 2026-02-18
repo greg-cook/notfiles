b696367e567927e93e0a3200c20b48d16ab3eef8
Greg Cook <5386965+greg-cook@users.noreply.github.com>
2026-02-18 21:35:12 +1100

    remove ghostty custom shader config

--- modules/home/programs/ghostty.nix
+++ modules/home/programs/ghostty.nix
@@ -7,8 +7,6 @@
     settings = {
       theme = "Catppuccin Mocha";
       copy-on-select = "clipboard";
-      # Disabled for now
-      # custom-shader = "~/.config/ghostty/shaders/shader.glsl";
     };
   };
 }
